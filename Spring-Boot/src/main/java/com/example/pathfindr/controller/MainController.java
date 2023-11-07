03db9096abb40557cf51b3c4e7f114917edc75a4
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-11-07 08:44:39 -0800

    error fixed

--- Spring-Boot/src/main/java/com/example/pathfindr/controller/MainController.java
+++ Spring-Boot/src/main/java/com/example/pathfindr/controller/MainController.java
@@ -1,18 +1,10 @@
 package com.example.pathfindr.controller;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.example.pathfindr.model.Role;
 import com.example.pathfindr.model.Student;
 import com.example.pathfindr.repository.RoleRepository;
 import com.example.pathfindr.repository.StudentRepository;
