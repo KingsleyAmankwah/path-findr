1a790c061a79129ecba6781f2f78296288f2e9b8
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-11-07 03:26:08 -0800

    files added
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

---
+++ Spring-Boot/src/main/java/com/example/pathfindr/PathfindrApplication.java
@@ -0,0 +1,13 @@
+package com.example.pathfindr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfindrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfindrApplication.class, args);
+	}
+
+}
