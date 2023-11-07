ed24f30098c49f20c249c336f202e63880d0425d
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-11-07 05:23:21 -0800

    Student signUp added

---
+++ Spring-Boot/src/main/java/com/example/pathfindr/service/validationService/ValidationService.java
@@ -0,0 +1,13 @@
+package com.example.pathfindr.service.validationService;
+
+public interface ValidationService {
+
+    void validateToken(String token);
+
+    void validatePassword(String password);
+
+    void validateEmail(String email);
+
+    void validateUsername(String username);
+
+}
