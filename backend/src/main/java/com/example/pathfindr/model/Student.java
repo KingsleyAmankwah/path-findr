7d583bfc2c08a0431393d11082d51614ba6da8ea
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-11-09 04:47:54 +0000

    home ...

--- backend/src/main/java/com/example/pathfindr/model/Student.java
+++ backend/src/main/java/com/example/pathfindr/model/Student.java
@@ -139,4 +139,15 @@ public class Student implements UserDetails {
         this.surveyResponse = surveyResponse;
     }
 
+    @Column(name = "specialty", nullable = false)
+    private String specialty;
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
 }
