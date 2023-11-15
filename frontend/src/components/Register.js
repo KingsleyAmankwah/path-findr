e0f976d07e113a943c4444c690c19177fcfbae3a
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2023-11-15 12:14:27 -0800

    ...

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -38,7 +38,7 @@ function SignUp() {
         body: JSON.stringify(Student),
       }
     );
-    const data = await response.json();
+    const data = await response.text();
     if (response.status === 200) {
       toast.success(`${data}`);
       navigate("/survey");
